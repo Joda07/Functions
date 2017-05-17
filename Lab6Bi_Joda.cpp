9676ccb09740f8f23b18329fe5ab220ba83dd80c
Emmanuel Joda <ijoda@bulldogs.aamu.edu>
2017-05-17 13:49:44 -0500

    Add files via upload
    Using The Square-root function to calculate the Square-roots of values

---
+++ Lab6Bi_Joda.cpp
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////
+// Name:Emmanuel Joda	
+// Date:03/23/17
+// Description: Calculating Using the Square root function
+///////////////////////////////////////////////////////
+
+#include <iostream> //Includes the iostream library
+#include <string> //Includes the string library
+#include <iomanip>    //for setw and setprecision
+#include <cmath>
+
+using namespace std; //Assume the use of the standard (std) family
+
+int main()  //the main function
+{ //Start of main
+
+	double userinput, result;
+	cout << "Please enter a number to calculate the square root: ";
+ 
+cin >> userinput;
+
+  
+result = sqrt (userinput);
+ 
+ cout << "The Square root of " << userinput << " is equal to " << result << endl;
+
+
+	
+	system("pause");  //Prevents the console from closing on exit.  This line is
+			  //not needed in Xcode (Mac OS X)
+	return 0;  //Returns whatever is defined by the function return type
+
+} //End of main
