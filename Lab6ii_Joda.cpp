eb9bb8abd5ac1896ca03388adb833744b0712fc2
Emmanuel Joda <ijoda@bulldogs.aamu.edu>
2017-05-17 14:06:09 -0500

    Add files via upload
    Calculating cube-root using Power functions

---
+++ Lab6ii_Joda.cpp
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////
+// Name:Emmanuel Joda
+// Date:30/03/17
+// Description:Finding the CUbe-root of a number using the Power function
+///////////////////////////////////////////////////////
+
+#include <iostream> 
+#include <string> 
+#include <iomanip>    
+using namespace std; 
+
+int main()  //the main function
+{ //Start of main
+
+
+
+
+int base = 0;
+
+int exp1=0.33333;
+
+double result = pow(base, exp1);
+
+for(int i = 1; i <= 10; i++)
+
+{
+
+
+	
+result = pow(i, 0.3333);
+
+	cout << result << endl;
+
+}
+
+	
+
+
+system("pause");
+
+return 0;
+
+
+}
